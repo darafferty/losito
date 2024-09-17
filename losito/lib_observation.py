2a8b1a5fc5725c86eb9dd8fef9472e98cfd1d087
David Rafferty <drafferty@hs.uni-hamburg.de>
2024-09-17 11:20:55 +0200

    Add initial support for wgridderpredict

--- losito/lib_observation.py
+++ losito/lib_observation.py
@@ -6,8 +6,11 @@ import subprocess
 import casacore.tables as pt
 import numpy as np
 from astropy.time import Time
+from astropy.io import fits
 import lsmtool
 from .lib_io import logger
+import sys
+import ast
 
 
 class MS:
@@ -84,10 +87,10 @@ class MS:
         self.channelwidth = tab.SPECTRAL_WINDOW.getcol('CHAN_WIDTH')[0]
 
         # Get pointing info
-        self.ra = np.degrees(float(tab.FIELD.getcol('REFERENCE_DIR')[0][0,0]))
+        self.ra = np.degrees(float(tab.FIELD.getcol('REFERENCE_DIR')[0][0, 0]))
         if self.ra < 0.:
             self.ra = 360.0 + (self.ra)
-        self.dec = np.degrees(float(tab.FIELD.getcol('REFERENCE_DIR')[0][0,1]))
+        self.dec = np.degrees(float(tab.FIELD.getcol('REFERENCE_DIR')[0][0, 1]))
 
         # Get station names, positions, and diameter
         self.stations = tab.ANTENNA.getcol('NAME')
@@ -100,7 +103,7 @@ class MS:
         if self.antennatype not in ['LBA_OUTER', 'LBA_INNER', 'LBA_SPARSE_EVEN', 'LBA_SPARSE_ODD', 'LBA_ALL',
                                     'HBA_DUAL_INNER']:
             logger.error('Antenna type not recognized (only LBA and HBA data '
-                             'are supported)')
+                         'are supported)')
         tab.close()
 
         # Find mean elevation and FOV
@@ -134,29 +137,50 @@ class Observation:
     ms_filenames : list of strings
         Filenames of the MS files.
     skymodel_filename : str
-        Filename of the sky model file.
+        Filename of the sky model file. The file can be a text file in makesourcedb
+        format or a FITS image.
+    regions_filename : str
+        Filename of the ds9 regions file that defines the directions to use. Only
+        needed when the input sky model is a FITS image.
     """
-    def __init__(self, ms_filenames, skymodel_filename=None, scheduler=None):
+    def __init__(self, ms_filenames, skymodel_filename=None, regions_filename=None,
+                 scheduler=None):
         if isinstance(ms_filenames, str):
             self.ms_filenames = [ms_filenames]
         else:
             self.ms_filenames = ms_filenames
-
-        self.input_skymodel_filename = skymodel_filename
-        self.output_skymodel_filename = skymodel_filename+'.losito'
-        self.sourcedb_filename = self.output_skymodel_filename + '.sourcedb'
         self.name = os.path.basename(self.ms_filenames[0][0:-9])
-
         self.scheduler = scheduler
+
+        # Check sky model
+        self.input_skymodel_filename = skymodel_filename
+        try:
+            fits.open(self.input_skymodel_filename)
+            self.input_skymodel_type = 'fitsimage'
+            self.output_skymodel_filename = None
+            self.sourcedb_filename = None
+        except OSError:
+            self.input_skymodel_type = 'makesourcedb'
+            self.output_skymodel_filename = skymodel_filename+'.losito'
+            self.sourcedb_filename = self.output_skymodel_filename + '.sourcedb'
+            self.regions_filename = None
+        if self.input_skymodel_type == 'fitsimage' and regions_filename is None:
+            logger.critical("For FITS sky models, a ds9 region file that specifies "
+                            "the directions (facets) must be supplied.")
+            sys.exit(1)
+        self.regions_filename = regions_filename
 
         # Load the sky model
         if skymodel_filename is not None:
             self.load_skymodel()
+
+        # Check MS files
         logger.info('Checking MS files')
         self.ms_list = [MS(_file) for _file in
                         self.ms_filenames]
-        self.set_time() # Set and test time information from MSs
-        self.set_stations() # Set station information from MSs
+        self.set_time()  # Set and test time information from MSs
+        self.set_stations()  # Set station information from MSs
+
         # Initialize the parset
         self.parset_filename = 'DP3_predict.parset'
         self.parset_parameters = {}
@@ -181,29 +205,30 @@ class Observation:
         """
         Makes the sourcedb for DP3 from the sky model
         """
-        self.save_skymodel()
-        cmd = ['makesourcedb', 'in={}'.format(self.output_skymodel_filename),
-               'out={}'.format(self.sourcedb_filename),
-               'format=<', 'outtype=blob', 'append=False']
-        subprocess.call(cmd)
-        # logger.debug('Copy sourcedb to all MS.')
-        # for ms in self:
-        #     subprocess.call('cp {} {}'.format(self.sourcedb_filename, ms.ms_filename))
+        if self.input_skymodel_type == 'makesourcedb':
+            self.save_skymodel()
+            cmd = ['makesourcedb', 'in={}'.format(self.output_skymodel_filename),
+                   'out={}'.format(self.sourcedb_filename),
+                   'format=<', 'outtype=blob', 'append=False']
+            subprocess.call(cmd)
 
     def load_skymodel(self):
         """
         Loads the sky model
         """
-        # Set logging level to suppress confusing output from lsmtool
-        old_level = logger.root.getEffectiveLevel()
-        logger.root.setLevel('WARNING')
-        skymodel = lsmtool.load(self.input_skymodel_filename)
-        if not skymodel.hasPatches:
-            logger.info('No patches present in skymodel. Assigning every source an individual patch.')
-            skymodel.group('every')
-            skymodel.setPatchPositions(method='mid')
-        logger.root.setLevel(old_level)
-        self.skymodel = skymodel
+        if self.input_skymodel_type == 'makesourcedb':
+            # Set logging level to suppress confusing output from lsmtool
+            old_level = logger.root.getEffectiveLevel()
+            logger.root.setLevel('WARNING')
+            skymodel = lsmtool.load(self.input_skymodel_filename)
+            if not skymodel.hasPatches:
+                logger.info('No patches present in skymodel. Assigning every source an individual patch.')
+                skymodel.group('every')
+                skymodel.setPatchPositions(method='mid')
+            logger.root.setLevel(old_level)
+            self.skymodel = skymodel
+        else:
+            self.skymodel = None
 
     def save_skymodel(self, filename=None, format='makesourcedb'):
         """
@@ -225,22 +250,32 @@ class Observation:
                 - 'factor'
                 - plus all other formats supported by the astropy.table package
         """
-        if filename is None:
-            filename = self.output_skymodel_filename
-        self.skymodel.write(filename, format=format, clobber=True)
+        if self.input_skymodel_type == 'makesourcedb':
+            if filename is None:
+                filename = self.output_skymodel_filename
+            self.skymodel.write(filename, format=format, clobber=True)
 
     def get_patch_coords(self):
         """
         Returns arrays of flux-weighted mean RA, Dec in degrees for patches in the
         sky model
         """
-        return self.skymodel.getPatchPositions(asArray=True)
+        if self.input_skymodel_type == 'makesourcedb':
+            coords = self.skymodel.getPatchPositions(asArray=True)
+        else:
+            ra, dec, _ = self.read_ds9_region_file(self.regions_filename)
+            coords = (ra, dec)
+        return coords
 
     def get_patch_names(self):
         """
         Returns list of DP3-compatible patch names
         """
-        patch_names = ['[{}]'.format(p) for p in self.skymodel.getPatchNames()]
+        if self.input_skymodel_type == 'makesourcedb':
+            patch_names = ['[{}]'.format(p) for p in self.skymodel.getPatchNames()]
+        else:
+            _, _, facet_names = self.read_ds9_region_file(self.regions_filename)
+            patch_names = ['[{}]'.format(p) for p in facet_names]
         return patch_names
 
     def initialize_parset_parameters(self):
@@ -259,7 +294,7 @@ class Observation:
             for k, v in self.parset_parameters.items():
                 f.write('{0} = {1}\n'.format(k, v))
 
-    def add_to_parset(self, stepname, soltabname, h5parmFilename = 'corruptions.h5', DDE=True):
+    def add_to_parset(self, stepname, soltabname, h5parmFilename='corruptions.h5', DDE=True):
         """
         Add the corruptions of a h5parm to a applycal step in the DP3 parset.
 
@@ -277,17 +312,16 @@ class Observation:
             self.parset_parameters[applyprefix + '.parmdb'] = h5parmFilename
             self.parset_parameters[applyprefix + '.correction'] = soltabname
         else:
-            if not 'applycal' in self.parset_parameters['steps']:
+            if 'applycal' not in self.parset_parameters['steps']:
                 self.parset_parameters['steps'].append('applycal')
             self.parset_parameters[applyprefix + '.invert'] = 'false'
             self.parset_parameters['applycal.type'] = 'applycal'
         if applyprefix+'.steps' in self.parset_parameters:
-                self.parset_parameters[applyprefix+'.steps'].append(stepname)
+            self.parset_parameters[applyprefix+'.steps'].append(stepname)
         else:
             self.parset_parameters[applyprefix+'.steps'] = [stepname]
         self.parset_parameters[applyprefix+'.{}.correction'.format(stepname)] = soltabname
         self.parset_parameters[applyprefix+'.{}.parmdb'.format(stepname)] = h5parmFilename
-
 
     def set_time(self):
         """ Set the time information. Also check wheter all the MS have
@@ -299,7 +333,7 @@ class Observation:
             timepersample.append(ms.timepersample)
             numsamples.append(ms.numsamples)
         if np.any([len(np.unique(_tms)) > 1 for _tms in [starttime, endtime,
-                                                timepersample, numsamples]]):
+                                                         timepersample, numsamples]]):
             logger.critical("Time information of MS {} does not match!".format(
                              ms.ms_filename))
         else:
@@ -330,7 +364,6 @@ class Observation:
         self.stations = np.array(stations)
         self.stationpositions = np.array(stationpositions)
 
-
     def reset_beam_keyword(self, colname='DATA'):
         """
         Unsets the LOFAR_APPLIED_BEAM_MODE keyword for the given column
@@ -344,7 +377,6 @@ class Observation:
             with ms.table(readonly=False) as t:
                 if colname in t.colnames() and 'LOFAR_APPLIED_BEAM_MODE' in t.getcolkeywords(colname):
                     t.putcolkeyword(colname, 'LOFAR_APPLIED_BEAM_MODE', 'None')
-
 
     @staticmethod
     def convert_mjd(mjd_sec):
@@ -368,3 +400,43 @@ class Observation:
         month = d.ctime().split(' ')[1]
 
         return '{0}{1}{2}/{3}'.format(day, month, year, hour)
+
+    @staticmethod
+    def read_ds9_region_file(region_file):
+        """
+        Read a ds9 facet region file and return facet coordinates and names
+
+        Parameters
+        ----------
+        region_file : str
+            Filename of input ds9 region file
+
+        Returns
+        -------
+        facet_ra, facet_dec, facet_name : Numpy arrays
+            Arrays of Facet coordinates and names
+        """
+        facet_ra = []
+        facet_dec = []
+        facet_name = []
+
+        with open(region_file, 'r') as f:
+            lines = f.readlines()
+        for line in lines:
+            # Each facet in the region file is defined by two consecutive lines:
+            #   - the first starts with 'polygon' and gives the (RA, Dec) vertices
+            #   - the second starts with 'point' and gives the reference (RA, Dec)
+            #     and the facet name
+            if line.startswith('polygon'):
+                continue
+            if line.startswith('point'):
+                ra, dec = ast.literal_eval(line.split('point')[1])
+                if 'text' in line:
+                    name = line.split('text=')[1].strip()
+                else:
+                    name = f'facet_{ra}_{dec}'
+                facet_ra.append(ra)
+                facet_dec.append(dec)
+                facet_name.append(name)
+
+        return np.array(facet_ra), np.array(facet_dec), np.array(facet_name)
